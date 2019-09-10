c9fed83e9d145186444c5667379b68c5f5bfbfc1
Ritika94168 <ritikagaba2501@gmail.com>
2019-09-10 22:32:45 +0530

    E

---
+++ app/src/main/java/com/example/myapplicationsens/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.myapplicationsens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+Sensor sensor;
+SensorManager sendorManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sendorManager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        sensor=sendorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+    @Override
+    public void onResume() {
+super.onResume();
+sendorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        sendorManager.unregisterListener(this);
+    }
+    MediaPlayer mp;
+    boolean isrunning=false;
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Toast.makeText(getApplicationContext(), ""+sensorEvent.values[0], Toast.LENGTH_SHORT).show();
+        if(sensorEvent.values[0]>5 && isrunning==false){
+            isrunning=true;
+mp=new MediaPlayer();
+try {
+    mp.setDataSource("http://server6.mp3quran.net/thubti/001.mp3");
+    mp.prepare();
+    mp.start();
+
+} catch (IOException e) {
+    e.printStackTrace();
+}
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
